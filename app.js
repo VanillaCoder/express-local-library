856358ea9e97f86b575d49a699ab715bc70c5de7
Leo Hearts <LiamHearts@gmail.com>
2023-01-07 12:21:03 -0800

    Schema/models/DB connected/created

--- app.js
+++ app.js
@@ -4,10 +4,18 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// Connect to MongoDB
+const mongoose = require('mongoose');
+const mongoDB = "mongodb+srv://admin:Portal99@cluster1.cv2yqzq.mongodb.net/local-library?retryWrites=true&w=majority"
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
